2d35a0f7137eaffced1dd50b7420c0a810344d2e
Carlos Gomez <gomezmendozacarlos>
2025-05-05 17:46:24 -0600

    Adds shopify integration, api keys and get api call

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,18 @@
-function MainProducts() {
+const getProducts  = async () => {  
+  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-04/products.json`, {
+    method: 'GET',
+    headers: {
+      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || '',
+      'Content-Type': 'application/json',
+    },  
+  });
+  const data = await response.json();
+  return data;
+}
+
+async function MainProducts() {
+  const products = await getProducts();
+  console.log(products);
   return (
     <h2>
       Main Products
